da7baa715fc45a7f49ac822d9efa16dd2dbd89b8
Lisa_und_Michi <lordrevan1999@web.de>
2022-12-06 22:47:27 +0100

    AOC-2015 02-12/2 solution to riddle 2/2

---
+++ src/main/java/de/barryallenofearth/adventofcode2015/riddle/b/part2/common/RibbonCalculator.java
@@ -0,0 +1,8 @@
+package de.barryallenofearth.adventofcode2015.riddle.b.part2.common;
+
+public class RibbonCalculator {
+
+	public static int calculateRequiredRibbon(int[] boxDimensions) {
+		return boxDimensions[0] * 2 + boxDimensions[1] * 2 + boxDimensions[0] * boxDimensions[1] * boxDimensions[2];
+	}
+}
