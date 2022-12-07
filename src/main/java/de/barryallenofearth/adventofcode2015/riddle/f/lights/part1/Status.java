5209731320790c9376899af3e862bc7e307c2552
Lisa_und_Michi <lordrevan1999@web.de>
2022-12-07 22:08:02 +0100

    solution riddle 6 part1 and part2

---
+++ src/main/java/de/barryallenofearth/adventofcode2015/riddle/f/lights/part1/Status.java
@@ -0,0 +1,5 @@
+package de.barryallenofearth.adventofcode2015.riddle.f.lights.part1;
+
+public enum Status {
+	ON, OFF;
+}
---
+++ src/main/java/de/barryallenofearth/adventofcode2015/riddle/f/lights/part2/LightOperation.java
@@ -0,0 +1,29 @@
+package de.barryallenofearth.adventofcode2015.riddle.f.lights.part2;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.function.Function;
+
+@Getter
+@RequiredArgsConstructor
+public enum LightOperation {
+	TURN_ON(brightness -> brightness + 1, "turn on"),
+
+	TURN_OFF(brightness -> brightness == 0 ? 0 : brightness - 1, "turn off"),
+
+	TOGGLE(brightness -> brightness + 2, "toggle");
+
+	private final Function<Integer, Integer> switchingFunction;
+
+	private final String key;
+
+	public static LightOperation getByKey(String key) {
+		for (LightOperation value : LightOperation.values()) {
+			if (value.getKey().equals(key)) {
+				return value;
+			}
+		}
+		return null;
+	}
+}
