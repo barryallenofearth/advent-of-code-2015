90379a039a2cff6d376c92deab3d95107eff130d
Michael <lordrevan1999@web.de>
2022-12-11 20:42:17 +0100

    AOC-2015 12-12 solution riddle 12 part 1

---
+++ src/main/java/de/barryallenofearth/adventofcode2015/riddle/l/Main_12_1.java
@@ -0,0 +1,28 @@
+package de.barryallenofearth.adventofcode2015.riddle.l;
+
+import de.barryallenofearth.adventofcode2015.riddle.util.RiddleFileReader;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main_12_1 {
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		final List<String> strings = RiddleFileReader.readAllLines("riddle-12.txt");
+		final List<Integer> allIntegers = new ArrayList<>();
+		for (String string : strings) {
+			int currentSum = 0;
+			final String[] split = string.replaceAll("[^\\d\\-]+", "\n").split("\n");
+			for (String number : split) {
+				if (number.matches("-?\\d+")) {
+					final int integer = Integer.parseInt(number);
+					currentSum += integer;
+					allIntegers.add(integer);
+				}
+			}
+			System.out.println(currentSum);
+		}
+		System.out.println(allIntegers.stream().mapToInt(Integer::intValue).sum() + " is the total sum");
+	}
+}
