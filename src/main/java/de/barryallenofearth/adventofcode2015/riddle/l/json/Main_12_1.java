e5a1cd538e8a7d25443c1bec5e9fafa8d8cc4fbb
Lisa_und_Michi <lordrevan1999@web.de>
2022-12-11 22:42:33 +0100

    AOC-2015 12-12/2 solution riddle 12 part2

--- src/main/java/de/barryallenofearth/adventofcode2015/riddle/l/json/Main_12_1.java
+++ src/main/java/de/barryallenofearth/adventofcode2015/riddle/l/json/Main_12_1.java
@@ -1,4 +1,4 @@
-package de.barryallenofearth.adventofcode2015.riddle.l;
+package de.barryallenofearth.adventofcode2015.riddle.l.json;
 
 import de.barryallenofearth.adventofcode2015.riddle.util.RiddleFileReader;
 
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Main_12_1 {
 	public static void main(String[] args) throws IOException, URISyntaxException {
-		final List<String> strings = RiddleFileReader.readAllLines("riddle-12.txt");
+		final List<String> strings = RiddleFileReader.readAllLines("riddle-12.json");
 		final List<Integer> allIntegers = new ArrayList<>();
 		for (String string : strings) {
 			int currentSum = 0;
---
+++ src/main/java/de/barryallenofearth/adventofcode2015/riddle/l/json/Main_12_2.java
@@ -0,0 +1,91 @@
+package de.barryallenofearth.adventofcode2015.riddle.l.json;
+
+import de.barryallenofearth.adventofcode2015.riddle.util.RiddleFileReader;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main_12_2 {
+	public static final String RED = "\"red\"";
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+
+		final List<String> strings = RiddleFileReader.readAllLines("riddle-12.json");
+		final List<Integer> allIntegers = new ArrayList<>();
+		for (String string : strings) {
+			int currentSum = 0;
+			while (string.contains(RED)) {
+				final int redIndex = string.indexOf(RED);
+				int startIndexRect = findOpeningBraceIndex(string, redIndex);
+				int stopIndexRect = findClosingBraceIndex(string, redIndex);
+				final String blockContainingRed = string.substring(startIndexRect, stopIndexRect + 1);
+				System.out.println(blockContainingRed.replace(",", ",\n"));
+				if (!blockContainingRed.contains(RED)) {
+					continue;
+				}
+				string = string.replace(blockContainingRed, "");
+				System.out.println();
+			}
+
+			final String[] split = string.replaceAll("[^\\d\\-]+", "\n").split("\n");
+			for (String number : split) {
+				if (number.matches("-?\\d+")) {
+					final int integer = Integer.parseInt(number);
+					currentSum += integer;
+					allIntegers.add(integer);
+				}
+			}
+			System.out.println(currentSum);
+		}
+		System.out.println(allIntegers.stream().mapToInt(Integer::intValue).sum() + " is the total sum");
+	}
+
+	public static int findOpeningBraceIndex(String json, int redStartingIndex) {
+		int openingIndex = 0;
+		int curlyBracesNeeded = 1;
+		int rectBracesNeeded = 1;
+		int currentIndex = redStartingIndex - 1;
+		while (curlyBracesNeeded > 0 && rectBracesNeeded > 0) {
+			if (json.charAt(currentIndex) == '[') {
+				rectBracesNeeded--;
+				openingIndex = currentIndex;
+			} else if (json.charAt(currentIndex) == ']') {
+				rectBracesNeeded++;
+			}
+			if (json.charAt(currentIndex) == '{') {
+				curlyBracesNeeded--;
+				openingIndex = currentIndex;
+			} else if (json.charAt(currentIndex) == '}') {
+				curlyBracesNeeded++;
+			}
+			currentIndex--;
+		}
+		return openingIndex;
+	}
+
+	public static int findClosingBraceIndex(String json, int redStartingIndex) {
+		int closingIndex = 0;
+		int curlyBracesNeeded = 1;
+		int rectBracesNeeded = 1;
+		int currentIndex = redStartingIndex + 1;
+		while (curlyBracesNeeded > 0 && rectBracesNeeded > 0) {
+			if (json.charAt(currentIndex) == ']') {
+				rectBracesNeeded--;
+				closingIndex = currentIndex;
+			} else if (json.charAt(currentIndex) == '[') {
+				rectBracesNeeded++;
+			}
+			if (json.charAt(currentIndex) == '}') {
+				curlyBracesNeeded--;
+				closingIndex = currentIndex;
+			} else if (json.charAt(currentIndex) == '{') {
+				curlyBracesNeeded++;
+			}
+			currentIndex++;
+		}
+		return closingIndex;
+	}
+
+}
