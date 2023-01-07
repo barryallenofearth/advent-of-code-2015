2ca8c1b155cd618545611a51e7b830d475e74463
Lisa_und_Michi <lordrevan1999@web.de>
2023-01-07 20:11:15 +0100

    AOC-2015 14/1 solution to riddle 14 part 1

---
+++ src/main/java/de/barryallenofearth/adventofcode2015/riddle/day14/common/model/Reindeer.java
@@ -0,0 +1,21 @@
+package de.barryallenofearth.adventofcode2015.riddle.day14.common.model;
+
+import lombok.Data;
+
+@Data
+public class Reindeer {
+
+	private final String name;
+
+	//in km/s
+	private final int speed;
+
+	//in seconds
+	private final int flyingTime;
+
+	//in seconds
+	private final int restingTime;
+
+	private int travelledDistance = 0;
+
+}
---
+++ src/main/java/de/barryallenofearth/adventofcode2015/riddle/day14/part2/EachSecondSystem.java
@@ -0,0 +1,4 @@
+package de.barryallenofearth.adventofcode2015.riddle.day14.part2;
+
+public class EachSecondSystem {
+}
