8157c77d10968a886ff6bc79af3f07d0734976c1
Michael Evelt <m.evelt@lvm.de>
2023-01-12 16:07:34 +0100

    AoC-2015 20/2 solution to riddle 20 part 2

--- src/main/java/de/barryallenofearth/adventofcode2015/riddle/day20/elfpresents/part2/PresentDistributerHouseLimit.java
+++ src/main/java/de/barryallenofearth/adventofcode2015/riddle/day20/elfpresents/part2/PresentDistributerHouseLimit.java
@@ -2,43 +2,44 @@ package de.barryallenofearth.adventofcode2015.riddle.day20.elfpresents.part2;
 
 public class PresentDistributerHouseLimit {
 
-	public static final int HOUSES_VISITED_PER_ELF = 50;
+    public static final int HOUSES_VISITED_PER_ELF = 50;
+    public static final int PRESENTS_PER_HOUSE = 11;
 
-	public int firstHouseToMatchNumberOfPresents(int targetPresents) {
+    public int firstHouseToMatchNumberOfPresents(int targetPresents) {
 
-		int minHouseNumber = Math.max((int) (-0.5 + Math.sqrt(0.25 + targetPresents / 5.5)), 500_000);
-		System.out.println(minHouseNumber + " is the minimum house number possible.");
-		final int maxHouseNumber = targetPresents / 11 + targetPresents % 11;
-		System.out.println(maxHouseNumber + " is the maximum house number possible.");
-		for (int house = minHouseNumber; house < maxHouseNumber; house++) {
-			int presentsAtHouse = sumOfElves(house) * 11;
-			if (targetPresents <= presentsAtHouse) {
-				return house;
-			}
-			if (house % 10_000 == 0) {
-				System.out.println("house; " + house + ", presents: " + presentsAtHouse + ", diff: " + (targetPresents - presentsAtHouse));
-			}
-		}
-		return -1;
-	}
+        final int bruteForceLowerLimit = 770_000;
+        int minHouseNumber = Math.max((int) (-0.5 + Math.sqrt(0.25 + targetPresents / 5.5)), bruteForceLowerLimit);
+        System.out.println(minHouseNumber + " is the minimum house number possible.");
+        final int maxHouseNumber = targetPresents / PRESENTS_PER_HOUSE + targetPresents % PRESENTS_PER_HOUSE;
+        System.out.println(maxHouseNumber + " is the maximum house number possible.");
+        for (int house = minHouseNumber; house < maxHouseNumber; house++) {
+            int presentsAtHouse = sumOfElves(house) * PRESENTS_PER_HOUSE;
+            if (targetPresents <= presentsAtHouse) {
+                return house;
+            }
+            if (house % 10_000 == 0) {
+                System.out.println("house: " + house + ", presents: " + presentsAtHouse + ", diff: " + (targetPresents - presentsAtHouse));
+            }
+        }
+        return -1;
+    }
 
-	public int sumOfElves(int houseNumber) {
-		int sum = 0;
-		if (houseNumber <= HOUSES_VISITED_PER_ELF) {
-			sum++;
-		}
-		sum += houseNumber;
-		for (int elf = (int) Math.sqrt(houseNumber); elf < houseNumber; elf++)
+    public int sumOfElves(int houseNumber) {
+        int sum = 0;
+//        houses below 50 will not lead to the required result => skip
+//        if (houseNumber <= HOUSES_VISITED_PER_ELF) {
+//            sum++;
+//        }
+        sum += houseNumber;
+        final int start = (int) Math.max(Math.sqrt(houseNumber), houseNumber / HOUSES_VISITED_PER_ELF);
+        for (int elf = start; elf < houseNumber; elf++) {
 
-			if (houseNumber % elf == 0) {
-				if (elf * HOUSES_VISITED_PER_ELF <= houseNumber) {
-					sum += elf;
-				}
-				final int factor = houseNumber / elf;
-				if (factor * HOUSES_VISITED_PER_ELF <= houseNumber) {
-					sum += factor;
-				}
-			}
-		return sum;
-	}
+            if (houseNumber % elf == 0) {
+                sum += elf;
+                final int secondElf = houseNumber / elf;
+                sum += secondElf;
+            }
+        }
+        return sum;
+    }
 }
