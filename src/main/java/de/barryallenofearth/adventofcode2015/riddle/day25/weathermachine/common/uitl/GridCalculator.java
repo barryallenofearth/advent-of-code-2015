aa873b5c164731843fd2bf443ab38b82e988978d
Lisa_und_Michi <lordrevan1999@web.de>
2023-01-12 12:46:33 +0100

    AOC-2015 25/1 correctly rastering through grid

--- src/main/java/de/barryallenofearth/adventofcode2015/riddle/day25/weathermachine/common/uitl/GridCalculator.java
+++ src/main/java/de/barryallenofearth/adventofcode2015/riddle/day25/weathermachine/common/uitl/GridCalculator.java
@@ -2,37 +2,30 @@ package de.barryallenofearth.adventofcode2015.riddle.day25.weathermachine.common
 
 public class GridCalculator {
 
-    public long calculate(long[][] grid, int targetX, int targetY) {
+	public long calculate(long[][] grid, int targetX, int targetY) {
 
-        int x = 0, y = 0;
+		int x = 0, y = 0;
 
-        int maxY = y;
-        int maxX = x;
-        int previousX = x;
-        int previousY = y;
-        while (x != targetX || y != targetY) {
-            x = (x + 1) % (maxY + 1);
-            if (x > maxX) {
-                maxY = x;
-            }
-            if (y > 0) {
-                y--;
-            } else {
-                y = maxX + 1;
-            }
-            if (maxY < y) {
-                maxY = y;
-            }
-            final long nextValue = grid[previousX][previousY] + 1;
-            System.out.println(previousX + " " + previousY + " " + grid[previousX][previousY]);
-            System.out.println(x + " " + y + " " + nextValue);
-            System.out.println();
-            grid[x][y] = nextValue;
+		int maxY = y;
+		int previousX = 0;
+		int previousY = 0;
+		while (x != targetX || y != targetY) {
+			x = (x + 1) % (maxY + 1);
+			if (y > 0) {
+				y--;
+			} else {
+				y = ++maxY;
+			}
+			final long nextValue = grid[previousX][previousY] + 1;
+			System.out.println(previousX + " " + previousY + " " + grid[previousX][previousY]);
+			System.out.println(x + " " + y + " " + nextValue);
+			System.out.println();
+			grid[x][y] = nextValue;
 
-            previousX = x;
-            previousY = y;
-        }
+			previousX = x;
+			previousY = y;
+		}
 
-        return grid[x][y];
-    }
+		return grid[x][y];
+	}
 }
--- src/main/java/de/barryallenofearth/adventofcode2015/riddle/day25/weathermachine/common/uitl/GridInitializer.java
+++ src/main/java/de/barryallenofearth/adventofcode2015/riddle/day25/weathermachine/common/uitl/GridInitializer.java
@@ -8,26 +8,25 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GridInitializer {
-    public GridAndTarget getGrid() {
-        final List<String> strings = RiddleFileReader.readAllLines("riddle-25.txt");
-        long startingValue = Integer.parseInt(strings.get(0));
-        final String targetDescription = strings.get(1);
-        final Matcher matcher = Pattern.compile("To continue, please consult the code grid in the manual.  Enter the code at row (\\d+), column (\\d+).").matcher(targetDescription);
+	public GridAndTarget getGrid() {
+		final List<String> strings = RiddleFileReader.readAllLines("riddle-25.txt");
+		long startingValue = Integer.parseInt(strings.get(0));
+		final String targetDescription = strings.get(1);
+		final Matcher matcher = Pattern.compile("To continue, please consult the code grid in the manual.  Enter the code at row (\\d+), column (\\d+).").matcher(targetDescription);
 
+		int targetX = 0;
+		int targetY = 0;
 
-        int targetX = 0;
-        int targetY = 0;
+		if (matcher.matches()) {
+			//yes, the numbers are correct
+			targetX = Integer.parseInt(matcher.group(2)) - 1;
+			targetY = Integer.parseInt(matcher.group(1)) - 1;
+		}
 
-        if (matcher.matches()) {
-            //yes, the numbers are correct
-            targetX = Integer.parseInt(matcher.group(2)) - 1;
-            targetY = Integer.parseInt(matcher.group(1)) - 1;
-        }
+		int gridDimension = targetX + targetY + 1;
 
-        int gridDimension = targetX + targetY - 1;
-
-        final long[][] grid = new long[gridDimension][gridDimension];
-        grid[0][0] = startingValue;
-        return new GridAndTarget(grid, targetX, targetY);
-    }
+		final long[][] grid = new long[gridDimension][gridDimension];
+		grid[0][0] = startingValue;
+		return new GridAndTarget(grid, targetX, targetY);
+	}
 }
